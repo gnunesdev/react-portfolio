dfe675ef1d0b6b7b7cb444fbf4e87d9fbce3e479
gnunesdev <gnunes.programmer@gmail.com>
2023-05-07 22:37:30 -0300

    feat: updates portfolio to use app dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import { type NextPage } from "next";
-
-import Head from "next/head";
 import { Header } from "~/components/Header";
 import { Main } from "./home/components/Main";
 import { About } from "./home/components/About";
 import { Companies } from "./home/components/Companies";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <>
-      <Head>
-        <title>gnunes.dev</title>
-      </Head>
       <div className="bg-indigo-900 text-amber-100">
         <Header />
         <Main />
@@ -20,6 +14,4 @@ const Home: NextPage = () => {
       </div>
     </>
   );
-};
-
-export default Home;
+}
