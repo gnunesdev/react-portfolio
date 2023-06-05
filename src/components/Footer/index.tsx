a9a1f905d4adb01b3bb87acdbf716bdb0fa24717
gnunesdev <gnunes.programmer@gmail.com>
2023-06-04 22:44:37 -0300

    feat: adds contact and footer sections

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-indigo-950 p-4">
+      <span className="block">
+        Todos os direitos reservados © Guilherme Nunes Teixeira 2023
+      </span>
+    </footer>
+  );
+};
